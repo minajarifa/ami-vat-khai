fd3447c0c8a3aec33d5ca16ec0faf0e88d333fe3
Arifa Islam <minajmiha02@gmail.com>
2024-06-27 14:39:40 +0600

    handleUpdateBtn

--- 55-1-client/src/Pages/Update/Update.jsx
+++ 55-1-client/src/Pages/Update/Update.jsx
@@ -1,13 +1,64 @@
 import { useLoaderData } from "react-router-dom";
 
 const Update = () => {
-    const useLoadedData = useLoaderData();
-    console.log(useLoadedData);
-    return (
-        <div>
-            <h1>update user name {useLoadedData.name}</h1>
+  const LoadedData = useLoaderData();
+  console.log(LoadedData);
+  const handleUpdateBtn = event=>{
+    event.preventDefault();
+    const from = event.target;
+    const email = from.email.value;
+    const password = from.password.value;
+    const users = { email, password };
+    console.log(users);
+    console.log('updated')
+  }
+  return (
+    <div>
+      <h1>update user name {LoadedData.email}</h1>
+      <div className=" my-20">
+        <div class="hero-content flex-col ">
+          <div class="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+            <form onSubmit={handleUpdateBtn} class="card-body">
+              <div class="form-control">
+                <label class="label">
+                  <span class="label-text">Email</span>
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="email"
+                  class="input input-bordered"
+                  defaultValue={LoadedData?.email}
+                  required
+                />
+              </div>
+              <div class="form-control">
+                <label class="label">
+                  <span class="label-text">Password</span>
+                </label>
+                <input
+                  type="password"
+                  name="password"
+                  placeholder="password"
+                  class="input input-bordered"
+                  defaultValue={LoadedData?.password}
+                  required
+                />
+                <label class="label">
+                  <a href="#" class="label-text-alt link link-hover">
+                    Forgot password?
+                  </a>
+                </label>
+              </div>
+              <div class="form-control mt-6">
+                <input type="submit" value="Update" name="" id="" />
+              </div>
+            </form>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Update;
