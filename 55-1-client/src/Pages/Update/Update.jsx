e4e992234a2b786f6622edad7a1b1da3959ae450
Arifa Islam <minajmiha02@gmail.com>
2024-06-27 12:48:16 +0600

    update useLoadedData

--- 55-1-client/src/Pages/Update/Update.jsx
+++ 55-1-client/src/Pages/Update/Update.jsx
@@ -1,8 +1,11 @@
+import { useLoaderData } from "react-router-dom";
 
 const Update = () => {
+    const useLoadedData = useLoaderData();
+    console.log(useLoadedData);
     return (
         <div>
-            
+            <h1>update user name {useLoadedData.name}</h1>
         </div>
     );
 };
